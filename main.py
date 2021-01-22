56816dc1211a7dc4f7e6275e826e5a55d8b8e382
JustinInthaly21 <825134@mystma.org>
2021-01-22 19:37:10 +0000

    factororsum 1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#WRITE YOUR CODE IN THIS FILE
+def FactorSum(x, o):
+    
+    t = 0
+    
+    
+    for i in range (0, x):
+        t = t + i
+    
+
+   
+    a = 1
+   
+    for i in range (1, x + 1):
+        a = a * i
+
+
+    
+
+    if o == "Sum":
+        return t
+    else:
+        return a
+
+print(FactorSum(10, "Sum"))
